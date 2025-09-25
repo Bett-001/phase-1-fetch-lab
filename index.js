a5203dec36697a7283aed6a3b059cbc897bab636
Tonnie-debug-svg <tonniekbett@gmail.com>
2025-09-26 00:33:49 +0300

    Completed fetch lab

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Always return fetch so tests can access it
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((books) => renderBooks(books));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const p = document.createElement("p");
+    p.textContent = book.name;
+    main.appendChild(p);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+// call fetchBooks when the page loads
+document.addEventListener("DOMContentLoaded", fetchBooks);
